b294836276177cc9006a27906fe5946e54acc863
Ruslan Semenov <semjonv.ruslan@rambler.ru>
2023-04-25 03:22:09 +0400

    Create card and donation-related components with styles
    Designed and developed a card component along with card-row and card-col variations. Additionally, implemented donation-related components including donation-info and donation-id with associated styles.

---
+++ src/components/Card/CardCol.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card__col">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardCol",
+};
+</script>
---
+++ src/components/Card/CardDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card__details">
+    <div class="card__details-title">{{ title }}</div>
+
+    <div class="card__details-value">
+      <slot name="cardDetailValue"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardDetails",
+  props: {
+    title: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
