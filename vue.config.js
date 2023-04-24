7cdc676d9b93def12c52a42a7055d142398edf04
Ruslan Semenov <semjonv.ruslan@rambler.ru>
2023-04-24 11:16:05 +0400

    Set up project and configure webpack
    nstalled project dependencies, added fonts, configured webpack to bundle assets, and added icons.

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("@vue/cli-service");
+const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{ from: "src/assets/images", to: "assets/images" }],
+      }),
+      new SVGSpritemapPlugin("src/assets/icons/*.svg", {
+        output: {
+          filename: "assets/icons/sprite.svg",
+          svg4everybody: true,
+        },
+        sprite: {
+          prefix: "icon-",
+        },
+      }),
+    ],
+  },
+});
