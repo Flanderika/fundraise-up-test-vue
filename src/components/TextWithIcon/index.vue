e2db91b7772bdf219853b8bc96ecc87c81c2dc29
Ruslan Semenov <semjonv.ruslan@rambler.ru>
2023-04-25 02:01:36 +0400

    link and text-with-icons components
    Created a link component with associated styles and antext-with-icon component for displaying text with an accompanying icon.

---
+++ src/components/TextWithIcon/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-with-icon" :class="{ 'text-with-icon--green': colorGreen }">
+    <div v-if="iconPrepend" class="text-with-icon__icon">
+      <base-icon :iconName="iconPrepend" />
+    </div>
+
+    <div class="text-with-icon__text">
+      <slot />
+    </div>
+
+    <div v-if="iconAppend" class="text-with-icon__icon">
+      <base-icon :iconName="iconAppend" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseIcon from "../Icon";
+
+export default {
+  name: "TextWithIcon",
+  components: {
+    BaseIcon,
+  },
+  props: {
+    colorGreen: {
+      type: Boolean,
+      default: false,
+    },
+    iconPrepend: {
+      type: String,
+    },
+    iconAppend: {
+      type: String,
+    },
+  },
+};
+</script>
